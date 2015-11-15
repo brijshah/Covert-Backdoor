4d810d5d509abed87814aba1b764064e77e46ec5
Callum Styan <callumstyan@gmail.com>
2015-11-14 18:33:01 -0800

    encrypt and decrypt need byte literals

--- encryption.py
+++ encryption.py
@@ -3,9 +3,9 @@
 import triplesec
 
 def encrypt(data, password):
-    ciphertext = triplesec.encrypt(data, password)
+    ciphertext = triplesec.encrypt(bytes(data), bytes(password))
     return ciphertext
 
 def decrypt(data, password):
-    plaintext = triplesec.decrypt(data, password)
+    plaintext = triplesec.decrypt(bytes(data), bytes(password))
     return plaintext
