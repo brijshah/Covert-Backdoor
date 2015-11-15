b0b262733f15b45c7a77ccd28ba2156c89747547
Callum Styan <callumstyan@gmail.com>
2015-11-14 18:08:17 -0800

    move encryption functions to their own file

--- client.py
+++ client.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python
 
-import time, triplesec, configfile, logging
+import time, configfile, logging, encryption
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
-
-def encrypt(data):
-    ciphertext = triplesec.encrypt(data, 'top-secret-pw')
-    return ciphertext
-
-def decrypt(data):
-    plaintext = triplesec.decrypt(data, 'top-secret-pw')
-    return plaintext
 
 def portKnock():
     for knock in configfile.knock:
---
+++ encryption.py
@@ -0,0 +1,9 @@
+import triplesec
+
+def encrypt(data, password):
+    ciphertext = triplesec.encrypt(data, password)
+    return ciphertext
+
+def decrypt(data, password):
+    plaintext = triplesec.decrypt(data, password)
+    return plaintext
